d00d0dbf5e885d84757ab5a53eda0b589a80d8a8
raj <rajsharmakosi23@gmail.com>
2026-04-03 19:20:47 +0530

    delete files

--- BridgeLabz/OOP/scenario-based/EmployeeWageComputation/EmployeeWageApp/Exceptions/WageComputationException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace EmployeeWageApp.Exceptions
-{
-    public class WageComputationException : Exception
-    {
-        public WageComputationException(string message) : base(message) { }
-    }
-}
